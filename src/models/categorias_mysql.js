4bcddaf18af9645d4bda87f470910acfab22b88c
anonyblast <gustavoiafelix@gmail.com>
2022-05-07 21:00:37 -0300

    feat(criar-models): Model de Categorias

---
+++ src/models/categorias_mysql.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const db_myql = require('../database/db_mysql');
+
+const Categoria = db_myql.define('categorias', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    codigo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    titulo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    status: { // 0 - Inativo, 1 - Ativo
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+
+module.exports = Categoria;
