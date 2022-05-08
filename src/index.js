7b7603b60791cdb597011ed3973bc545912c4e4c
anonyblast <gustavoiafelix@gmail.com>
2022-05-07 21:27:21 -0300

    feat(inicializar-aplicacao): Adicionando função assíncrona de mapeamento do db

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 (async () => {
     const db_myql = require('./database/db_mysql');
-    const Produto = require('./models/produtos_mysql');
-    const Categoria = require('./models/categorias_mysql');
+    // const Produto = require('./models/produtos_mysql');
+    // const Categoria = require('./models/categorias_mysql');
 
     try {
         const result = await db_myql.sync({ force: true });
